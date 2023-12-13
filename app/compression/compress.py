df2132971dd8e45fb5e6fdd814e503ee16580a2d
KennedyRyan-code <kenryan49@gmail.com>
2023-12-13 12:29:27 +0300

    pycodestyle modifications

--- app/compression/compress.py
+++ app/compression/compress.py
@@ -2,10 +2,11 @@
 
 from PIL import Image
 
+
 def compress_image(image_path, output_path, quality=50):
     try:
-	img = Image.open(image_path)
-	img.save(output_path, quality=quality)
-	return True, "Image compressed successfully"
+        img = Image.open(image_path)
+        img.save(output_path, quality=quality)
+        return True, "Image compressed successfully"
     except Exception as e:
-	return False, f"Error: {e}"
+        return False, f"Error: {e}"
--- app/compression/decompress.py
+++ app/compression/decompress.py
@@ -2,10 +2,11 @@
 
 from PIL import Image
 
+
 def decompress_image(compressed_image_path, output_path):
     try:
-	compressed_img = Image.open(compressed_image_path)
-	compressed_img.save(output_path)
-	retun True, "Image decompressed successfully"
+        compressed_img = Image.open(compressed_image_path)
+        compressed_img.save(output_path)
+        return True, "Image decompressed successfully"
     except Exception as e:
-	return False, f"Error: {e}"
+        return False, f"Error: {e}"
